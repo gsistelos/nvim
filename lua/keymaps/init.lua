7d3241310d90b622a0498286736e8c52494aef43
gsistelos <gabrielsistelos@gmail.com>
2023-10-28 18:40:46 -0300

    change: NERDTreeToggle keymap

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<C-b>', ':NERDTreeToggle<Enter>')
+vim.keymap.set('n', '<leader>b', ':NERDTreeToggle<Enter>')
 
 -- keymaps for nvim-cmp at after/plugin/lspconfig.lua
