450754cd8252f1dad60d1427fdef17fe82a6a2be
gsistelos <gabrielsistelos@gmail.com>
2023-10-28 18:38:35 -0300

    feat: keymaps for cmp

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -15,6 +15,11 @@ cmp.setup({
 			vim.fn['vsnip#anonymous'](args.body)
 		end,
 	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+	}),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
 		{ name = 'vsnip' },
--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -1,3 +1,5 @@
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<C-b>', ':NERDTreeToggle<Enter>')
+
+-- keymaps for nvim-cmp at after/plugin/lspconfig.lua
