8c70d56763140e0b9fb03a2c81ab552adf6e5137
gsistelos <gabrielsistelos@gmail.com>
2023-12-01 00:01:29 -0300

    feat: nvim-cmp mappings

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -6,12 +6,19 @@ require('mason-lspconfig').setup({
     ensure_installed = servers
 })
 
-require('cmp').setup({
+local cmp = require('cmp')
+
+cmp.setup({
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body)
         end
     },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true })
+    }),
     sources = {
         { name = 'nvim_lsp' },
         { name = 'buffer' },
