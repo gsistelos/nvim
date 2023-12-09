08a8df7de19db0831430cdd40bd9546ab081ebcb
gsistelos <gabrielsistelos@gmail.com>
2023-12-09 00:42:52 -0300

    feat: treesitter bash and python parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript' },
+    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'bash', 'python' },
     sync_install = false,
     auto_install = false, -- Install new parsers with :TSInstall
     highlight = {
