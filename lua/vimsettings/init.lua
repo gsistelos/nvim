99af077fbb70f97983d8de801ecfbbef70457069
gsistelos <gabrielsistelos@gmail.com>
2023-12-20 02:12:32 -0300

    feat: format on save with :W

--- lua/vimsettings/init.lua
+++ lua/vimsettings/init.lua
@@ -9,5 +9,6 @@ vim.cmd('set expandtab')
 -- Visual whitespaces
 vim.cmd('set listchars=space:•,tab:__,eol:⏎')
 
--- Format on save
-vim.cmd('autocmd BufWritePre * lua vim.lsp.buf.format()')
+-- Format on save with :W
+vim.cmd('command! Format :lua vim.lsp.buf.format()')
+vim.cmd('command! W :w | Format')
