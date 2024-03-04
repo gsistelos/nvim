390505266cbf323617cb7fa27726ab2cc7baaaa8
gsistelos <gabrielsistelos@gmail.com>
2024-03-04 18:06:35 -0300

    feat: telescope keymaps

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -2,3 +2,10 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>e', ':E<CR>')
 vim.keymap.set('n', '<leader>t', ':set list!<CR>')
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
