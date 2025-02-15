1f6ac8417789ff23bfdd39328e839903abc2298e
gsistelos <gsistelos@proton.me>
2025-02-15 20:32:56 -0300

    refactor gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -6,33 +6,35 @@ return {
 		attach_to_untracked = true,
 		-- Highlight line number
 		numhl = true,
-		signs = {},
-		on_attach = function(bufnr)
+		on_attach = function(buf)
 			local gitsigns = require('gitsigns')
 
-			local function map(mode, lhs, rhs, opts)
-				opts = opts or {}
-				opts.buffer = bufnr
-				vim.keymap.set(mode, lhs, rhs, opts)
+			local function map(mode, keys, func, desc)
+				vim.keymap.set(mode, keys, func, {
+					buffer = buf,
+					desc = desc
+				})
 			end
 
-			map('n', '<leader>gb', gitsigns.toggle_current_line_blame, { desc = 'Git blame' })
+			map('n', '<leader>gb', gitsigns.toggle_current_line_blame, 'Git blame')
 
-			map('n', '<leader>gn', gitsigns.next_hunk, { desc = 'Git next hunk' })
-			map('n', '<leader>gp', gitsigns.prev_hunk, { desc = 'Git previous hunk' })
+			map('n', '<leader>gn', gitsigns.next_hunk, 'Git next hunk')
+			map('n', '<leader>gp', gitsigns.prev_hunk, 'Git previous hunk')
 
-			map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'Git toggle stage hunk' })
-			map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'Git reset hunk' })
+			map('n', '<leader>gs', gitsigns.stage_hunk, 'Git toggle stage hunk')
+			map('n', '<leader>gr', gitsigns.reset_hunk, 'Git reset hunk')
 
 			map('v', '<leader>gs', function()
 				gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-			end, { desc = 'Git toggle stage hunk' })
+			end, 'Git toggle stage hunk')
 
 			map('v', '<leader>gr', function()
 				gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-			end, { desc = 'Git reset hunk' })
+			end, 'Git reset hunk')
 
-			map('n', '<leader>gd', gitsigns.toggle_deleted, { desc = 'Git toggle deleted' })
+			map('n', '<leader>gS', gitsigns.stage_buffer, 'Git stage buffer')
+
+			map('n', '<leader>gd', gitsigns.toggle_deleted, 'Git toggle deleted')
 		end,
 	},
 }
